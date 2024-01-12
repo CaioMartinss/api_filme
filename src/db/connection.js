980e70b78ad2ac0994dc56fe42b55ddb531ae28d
Caio Martins do Nascimento <martinscaio29@gmail.com>
2024-01-12 18:03:40 -0300

    conn

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
+import config from "../config/config";
 
-mongoose.connect("mongodb+srv://root:root@cluster0.p2pjdan.mongodb.net/?retryWrites=true&w=majority")
+mongoose.connect(config.mongoURI)
     .then(() => console.log('MongoDB Conectado'))
     .catch(err => {
         console.error('Erro de Conexão com o MongoDB:', err);
-        process.exit(1); // Encerra o processo em caso de erro de conexão
+        process.exit(1);
     });
+
